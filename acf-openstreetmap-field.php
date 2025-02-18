ce627efeaeeabebf8dd2f7d0b9bd4903314bf922
NiclasNorin <niclasn@outlook.com>
2025-02-18 08:28:58 +0100

    chore: adding plugin main file

---
+++ acf-openstreetmap-field.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Plugin Name:    Acf OpenStreetMap Field
+Description:    Open Street Map field for Advanced Custom Fields
+Version:        1.0
+Author:         Niclas Norin
+*/
+
+if (! defined('WPINC')) {
+    die;
+}
+
+define('ACFOPENSTREETMAP_PATH', plugin_dir_path(__FILE__));
+define('ACFOPENSTREETMAP_URL', plugins_url('', __FILE__));
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+}
+
+add_action( 'acf/include_field_types', 'addAcfOpenStreetMapField' );
+
+function addAcfOpenStreetMapField() {
+    require_once ACFOPENSTREETMAP_PATH . 'source/php/acf-open-street-map-field.php';
+}
+
+?>
