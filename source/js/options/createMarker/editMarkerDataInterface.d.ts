2b4384774d9dcb78ba47f61235610165c44bb213
NiclasNorin <niclasn@outlook.com>
2025-02-24 13:45:59 +0100

    refactor: remove old field validator and editmarker

--- source/js/options/createMarker/editMarkerDataInterface.d.ts
+++
@@ -1,8 +0,0 @@
-interface EditMarkerDataInterface {
-    setCurrentMarker(markerData: MarkerDataInterface): void;
-    showOverlay(): void;
-    hideOverlay(): void;
-    setTitleValue(value: string): void;
-    setUrlValue(value: string): void;
-    setDescriptionValue(value: string): void;
-}
