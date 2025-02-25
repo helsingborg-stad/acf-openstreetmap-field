4d15173736fdf2c25ff0fcae58bd7049bafde028
NiclasNorin <niclasn@outlook.com>
2025-02-25 15:43:11 +0100

    feat: layer group creator init

---
+++ source/js/options/createLayerGroup/optionCreateLayerGroup.ts
@@ -0,0 +1,22 @@
+class OptionCreateLayerGroup implements OptionFeature {
+    protected condition: string = 'create_layer_group';
+    constructor(
+        private container: HTMLElement,
+        private handleSelectedInstance: HandleSelectedInterface,
+    ) {
+        this.addListener();
+    }
+
+    private addListener() {
+        this.container.querySelector('[data-js-value="create_layer_group"]')?.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log("click");
+        });
+    }
+
+    public checkCondition(value: string): boolean {
+        return value === this.condition;
+    }
+}
+
+export default OptionCreateLayerGroup;
