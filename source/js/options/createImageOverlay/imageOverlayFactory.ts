6d674b99f2608c62210d734b8d014c2cf04ba7bf
NiclasNorin <niclasn@outlook.com>
2025-03-03 18:28:57 +0100

    fix: update factory

--- source/js/options/createImageOverlay/imageOverlayFactory.ts
+++ source/js/options/createImageOverlay/imageOverlayFactory.ts
@@ -1,16 +1,23 @@
 import { CreateImageOverlayInterface } from "../../../OpenStreetMap/js/features/createImageOverlay/createImageOverlayInterface";
+import { CreateMarkerInterface } from "../../../OpenStreetMap/js/features/createMarker/createMarkerInterface";
 import { CreateRectangleInterface } from "../../../OpenStreetMap/js/features/createRectangle/createRectangleInterface";
 import { MapInterface } from "../../../OpenStreetMap/js/mapInterface";
 import EditImageOverlayFactory from "./edit/editImageOverlayDataFactory";
+import ImageOverlayCalculator from "./helper/imageOverlayBoundsAndRatioCalculator";
+import { ImageOverlayBoundsAndRatioCalculatorInterface } from "./helper/imageOverlayBoundsAndRatioCalculatorInterface";
 import ImageOverlayData from "./imageOverlayData";
+import { ImageOverlayResizeInterface } from "./imageFunctionality/imageOverlayResizeInterface";
 
 class ImageOverlayFactory implements ImageOverlayFactoryInterface {
     constructor(
         private mapInstance: MapInterface,
         private createImageOverlayInstance: CreateImageOverlayInterface,
         private createRectangle: CreateRectangleInterface,
+        private createMarkerInstance: CreateMarkerInterface,
         private editImageOverlayFactoryInstance: EditImageOverlayFactory,
         private imageOverlaysListInstance: ImageOverlaysListInterface,
+        private ImageOverlayBoundsAndRatioCalculatorInstance: ImageOverlayBoundsAndRatioCalculatorInterface,
+        private imageOverlayResizeInstance: ImageOverlayResizeInterface
     ) {
 
     }
@@ -19,8 +26,11 @@ class ImageOverlayFactory implements ImageOverlayFactoryInterface {
             this.mapInstance,
             this.createImageOverlayInstance,
             this.createRectangle,
+            this.createMarkerInstance,
             this.editImageOverlayFactoryInstance,
-            this.imageOverlaysListInstance
+            this.imageOverlaysListInstance,
+            this.ImageOverlayBoundsAndRatioCalculatorInstance,
+            this.imageOverlayResizeInstance
         );
     }
 }
