f19b77dfff680be6c55aa06920ce6b744a9b7ccf
NiclasNorin <niclasn@outlook.com>
2025-03-04 16:10:03 +0100

    chore: replace with package references

--- source/js/options/createMarker/optionCreateMarker.ts
+++ source/js/options/createMarker/optionCreateMarker.ts
@@ -1,4 +1,4 @@
-import { MapInterface } from "../../../OpenStreetMap/js/mapInterface";
+import { MapInterface } from "@helsingborg-stad/openstreetmap";
 import { MarkerFactoryInterface } from "./markerFactoryInterface";
 
 class OptionCreateMarker implements OptionFeature {
