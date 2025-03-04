53571fbf06795db4d88897818e924a80d527fd1b
NiclasNorin <niclasn@outlook.com>
2025-03-04 13:22:26 +0100

    fix: updating image overlay when loading to set correct names

--- source/js/options/createImageOverlay/loadImageOverlays.ts
+++ source/js/options/createImageOverlay/loadImageOverlays.ts
@@ -16,6 +16,7 @@ class LoadImageOverlays implements LoadOptionDataInterface {
             imageOverlayData.setLayerGroup(savedImageOverlay.layerGroup ?? '');
             imageOverlayData.setImageAspectRatio(savedImageOverlay.aspectRatio ?? null);
             imageOverlayData.setImage(savedImageOverlay.image ?? '', savedImageOverlay.position ?? null);
+            imageOverlayData.updateImageOverlay();
         }
     }
 }
