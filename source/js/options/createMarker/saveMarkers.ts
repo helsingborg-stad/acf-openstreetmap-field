75622f8170f9e8de74087d2eca2782a86aa9a346
NiclasNorin <niclasn@outlook.com>
2025-03-04 12:22:09 +0100

    refactor: marker save data

--- source/js/options/createMarker/saveMarkers.ts
+++ source/js/options/createMarker/saveMarkers.ts
@@ -8,12 +8,16 @@ class SaveMarkers implements SaveOptionDataInterface {
     public save(): SavedMarkerData {
         let data = [];
         for (let marker of Object.values(MarkerData.getMarkers())) {
+            if (!marker.getMarker()) {
+                continue;
+            }
+
             data.push({
                 title: marker.getTitle(),
                 url: marker.getUrl(),
                 description: marker.getDescription(),
-                position: marker.getMarker()?.getPosition() ?? {lat: 0, lng: 0},
-                layerGroup: marker.getLayerGroup() ?? ''
+                position: marker.getMarker()!.getPosition(),
+                layerGroup: marker.getLayerGroup()
             });
         }
 
