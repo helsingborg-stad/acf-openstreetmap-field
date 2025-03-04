2d173bed13e354b5c9e46fac19c0c3984359932e
NiclasNorin <niclasn@outlook.com>
2025-03-04 16:10:00 +0100

    chore: replace with package references

--- source/js/options/createMarker/markersList.ts
+++ source/js/options/createMarker/markersList.ts
@@ -1,4 +1,4 @@
-import { MapInterface } from "../../../OpenStreetMap/js/mapInterface";
+import { MapInterface } from "@helsingborg-stad/openstreetmap";
 import { MarkerDataInterface } from "./markerDataInterface";
 import { MarkersListDataStorage, MarkersListInterface } from "./markersListInterface";
 import { createListItem } from "../../helper/createListItem";
