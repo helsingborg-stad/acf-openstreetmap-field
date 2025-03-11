693d5076538edcaa23d2c48ee10cee7e0497da44
NiclasNorin <niclasn@outlook.com>
2025-03-11 16:31:55 +0100

    feat: nullIconFacotry

--- source/js/icons/nullIconFactory.ts
+++ source/js/icons/nullIconFactory.ts
@@ -1,6 +1,19 @@
 class NullIconFactory implements IconFactoryInterface {
-    public create(icon: string, color: string): string {
-        return "";
+    private mapIconNameMap(icon: string): string|null {
+        const map: IconMap = {
+            'location': '📍',
+            'resize': '⇲',
+            'move': '❖',
+            'edit': '✎'
+        }
+
+        return map[icon as keyof IconMap] ?? null;
+    }
+
+    public create(icon: string, color: string, size: number = 20): string {
+        const iconName = this.mapIconNameMap(icon);
+
+        return `<span style="display: flex; justify-content: center; align-items: center; width: 24px; height: 24px; font-size: ${size}px; padding: 4px; background-color: ${color}; border-radius: 50%;">${iconName ?? '📍'}</span>`;
     }
 }
 
