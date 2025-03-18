86a6864463f0827ea3c470a57f03d17418f51679
NiclasNorin <niclasn@outlook.com>
2025-03-18 10:53:15 +0100

    refactor: remove old list item helper

--- source/js/helper/createListItem.ts
+++ source/js/helper/createListItem.ts
@@ -1,20 +1,6 @@
 class ListItemHelper {
     constructor(private iconFactoryInstance: IconFactoryInterface) {}
 
-    public createListItem(title: string, editText: string|null = null): HTMLLIElement {
-        const li = document.createElement('li');
-
-        const titleSpan = document.createElement('span');
-        titleSpan.textContent = title;
-        
-        const editIconSpan = this.createEditIcon(editText);
-
-        li.appendChild(titleSpan);
-        li.appendChild(editIconSpan);
-
-        return li;
-    }
-
     public createLayerGroupListItem(title: string): HTMLLIElement {
         const li = document.createElement('li');
         li.classList.add('button');
@@ -35,20 +21,6 @@ class ListItemHelper {
         li.innerHTML = `<span style="border-radius: 50%; width: .25rem; height: .25rem;">${title}</span>`;
 
         return li;
-    }
-
-
-    private createEditIcon(editText: string|null = null): HTMLSpanElement {
-        const span = document.createElement('span');
-        if (editText) {
-            span.innerHTML = `<span data-js-edit>${editText}</span>`;
-        } else {
-            span.setAttribute('data-js-edit', '');
-            span.style.display = 'inline-flex';
-            span.innerHTML = this.iconFactoryInstance.create('edit', '#000', 20, false);
-        }
-
-        return span;
     }
 }
 
