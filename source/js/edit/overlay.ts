0a43aff79b9fcecf5ca26e53dba6f7cf82d216fa
NiclasNorin <niclasn@outlook.com>
2025-03-19 11:43:17 +0100

    fix: close overlay on escape

--- source/js/edit/overlay.ts
+++ source/js/edit/overlay.ts
@@ -2,6 +2,16 @@ class Overlay implements OverlayInterface {
     private overlay: HTMLElement|null;
     constructor(private container: HTMLElement) {
         this.overlay = this.container.querySelector('[ data-js-field-edit-overlay]');
+
+        this.setListeners();
+    }
+
+    private setListeners(): void {
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                this.hideOverlay();
+            }
+        });
     }
 
     public getOverlay(): HTMLElement|null {
