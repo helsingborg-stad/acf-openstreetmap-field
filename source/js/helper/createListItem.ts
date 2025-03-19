b66b62e0f7f035a21ab025aecd5c3653f7b0a7b5
NiclasNorin <niclasn@outlook.com>
2025-03-19 11:43:10 +0100

    refactor: styling when hover edit layer

--- source/js/helper/createListItem.ts
+++ source/js/helper/createListItem.ts
@@ -4,7 +4,7 @@ class ListItemHelper {
     public createLayerGroupListItem(title: string): HTMLLIElement {
         const li = document.createElement('li');
         li.classList.add('button');
-        li.innerHTML = `<span style="display: flex; justify-content: space-between; align-items: center; width: 100%;"><span data-js-title>${title}</span><span data-js-edit>edit</span></span><div class="line-horizontal arrow-right"></div>`;
+        li.innerHTML = `<span style="display: flex; justify-content: space-between; align-items: center; width: 100%;"><span data-js-title>${title}</span><span class="acf-openstreetmap__edit-layer-group" data-js-edit>edit</span></span><div class="line-horizontal arrow-right"></div>`;
         return li;
     }
 
