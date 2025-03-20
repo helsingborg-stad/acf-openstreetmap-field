f1aa0a89209ac489c6646121dd068b954439a5de
GitHub Action <>
2025-03-20 13:05:05 +0000

    build: update package file versions to 0.75.10

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'helsingborg-stad/acf-openstreetmap-field',
-        'pretty_version' => '0.75.9',
-        'version' => '0.75.9.0',
+        'pretty_version' => '0.75.10',
+        'version' => '0.75.10.0',
         'reference' => null,
         'type' => 'wordpress-muplugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'helsingborg-stad/acf-openstreetmap-field' => array(
-            'pretty_version' => '0.75.9',
-            'version' => '0.75.9.0',
+            'pretty_version' => '0.75.10',
+            'version' => '0.75.10.0',
             'reference' => null,
             'type' => 'wordpress-muplugin',
             'install_path' => __DIR__ . '/../../',
