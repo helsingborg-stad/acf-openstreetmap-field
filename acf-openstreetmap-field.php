94228acdc4d1b2c86bf604ec96066ed72b7d7ee4
NiclasNorin <niclasn@outlook.com>
2026-09-03 15:15:24 +0200

    fix: updating to vite and enqueue using

--- acf-openstreetmap-field.php
+++ acf-openstreetmap-field.php
@@ -7,41 +7,40 @@ Version:        1.0
 Author:         Niclas Norin
 */
 
-use AcfOpenStreetMap\CacheBust;
+use WpService\Implementations\NativeWpService;
+use WpUtilService\WpUtilService;
 
 if (! defined('WPINC')) {
     die;
 }
 
-define('ACFOPENSTREETMAP_PATH', plugin_dir_path(__FILE__));
-define('ACFOPENSTREETMAP_URL', plugins_url('', __FILE__));
+$wpService = new NativeWpService();
+$wpUtils = new WpUtilService($wpService);
+
+define('ACFOPENSTREETMAP_PATH', $wpService->pluginDirPath(__FILE__));
+define('ACFOPENSTREETMAP_URL', $wpService->pluginsUrl('', __FILE__));
 
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
 }
 
-add_action('init', function () {
+$wpService->addAction('init', function () use ($wpService) {
     $domain = 'acf-openstreetmap-field';
-    $locale = determine_locale();
-    $mofile = plugin_dir_path(__FILE__) . 'languages/' . $domain . '-' . $locale . '.mo';
+    $locale = $wpService->determineLocale();
+    $mofile = $wpService->pluginDirPath(__FILE__) . 'languages/' . $domain . '-' . $locale . '.mo';
 
-    load_textdomain($domain, $mofile);
+    $wpService->loadTextdomain($domain, $mofile);
 });
 
-add_action( 'acf/include_field_types', 'addAcfOpenStreetMapField');
+$wpService->addAction( 'acf/include_field_types', 'addAcfOpenStreetMapField');
 
-add_action('admin_enqueue_scripts', 'loadScriptsAndStyle', 10);
-add_action('enqueue_block_editor_assets', 'loadScriptsAndStyle', 10);
 
-function getCacheBust() {
-    static $cacheBust = null;
-
-    if ($cacheBust === null) {
-        $cacheBust = new CacheBust();
-    }
-
-    return $cacheBust;
+foreach (['admin_enqueue_scripts', 'enqueue_block_editor_assets'] as $hook) {
+    $wpService->addAction($hook, function() use ($wpUtils, $hook) {
+        loadScriptsAndStyle($wpUtils, $hook);
+    });
 }
+
 
 function addAcfOpenStreetMapField() {
     require_once ACFOPENSTREETMAP_PATH . 'source/php/field.php';
@@ -50,25 +49,12 @@ function addAcfOpenStreetMapField() {
 /**
  * Enqueue scripts and styles in the admin
  */
-function loadScriptsAndStyle() {
-
-    wp_register_style(
-        'css-main',
-        ACFOPENSTREETMAP_URL . '/dist/' .
-        getCacheBust()->name('css/main-map.css')
-    );
-
-    wp_register_script(
-        'js-init-map',
-        ACFOPENSTREETMAP_URL . '/dist/' .
-        getCacheBust()->name('js/init-map.js'),
-        array('acf-input', 'jquery'),
-    );
-
-    wp_localize_script('js-init-map', 'language', \AcfOpenStreetMap\Lang::getLang());    
-
-    wp_enqueue_script('js-init-map');
-    wp_enqueue_style('css-main');
+function loadScriptsAndStyle(WpUtilService $wpUtils, string $hook) {
+    $array = \AcfOpenStreetMap\Lang::getLang();
+    $wpUtils
+        ->enqueue(__DIR__)
+        ->add('css/main.css')
+        ->add('js/init.js', ['acf-input', 'jquery'])
+        ->with()
+        ->translation('abcdef', $array);
 }
-
-?>
